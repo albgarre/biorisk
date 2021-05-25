7ea76d9e9b5e4f715c099ed60af4e47991df0b29
Arícia Possas <ariciamp@gmail.com>
2021-05-25 21:09:12 +0200

    I created the Weibull inactivation model module

--- R/inactivation.R
+++ R/inactivation.R
@@ -58,10 +58,76 @@ LogLinInactivation <- R6::R6Class(
 
 )
 
+#' R6 class describing the Weibull inactivation
+#'
+#' @details
+#' A risk module describing Weibullian inactivation. It has 4 inputs: logN0, t
+#' log_delta, beta.
+#'
+#' @export
+#'
+WeibullInactivation <- R6::R6Class(
+  classname = "WeibullInactivation",
+  inherit = RiskModule,
+  public = list(
+    
+    initialize = function(name,
+                          units = NA,
+                          output_unit = NA) {
+      
+      super$initialize(name,
+                       input_names = c("t", "log_delta", "beta", "logN0"),
+                       units = units,
+                       module_type = "inactivation",
+                       output_var = "logN",
+                       output_unit = output_unit)
+      
+    },
+    
+    
+    #' @description
+    #' Simulates the module
+    #' @param niter Number of Monte Carlo simulations.
+    #' @return the output of the module
+    #'
+    simulate = function(niter) {
+      
+      ## Do the simulations (recursively)
+      
+      sims <- tibble::tibble(
+        t = self$depends_on$t$simulate(niter),
+        log_delta = self$depends_on$log_delta$simulate(niter),
+        logN0 = self$depends_on$logN0$simulate(niter),
+        beta = self$depends_on$beta$simulate(niter)
+      ) %>%
+        dplyr::mutate(
+          logN = logN0 - (t/(10^log_delta))^beta
+        )
+      
+      ## Save the results of the simulations
+      
+      self$simulations <- sims
+      
+      ## Return
+      
+      sims$logN
+      
+    }
+    
+  )
+  
+)
+
+
 ## tests
 
 # time <- Constant$new("Time", 30)
+# time2 <- Constant$new("Time", 30, output_unit = "CFU")
 #
+# time$output_unit
+# time2$output_unit
+
+
 # logD <- Normal$new("logD")$
 #   map_input("mu", Constant$new("mu", 1))$
 #   map_input("sigma", Constant$new("sigma", 0.2))
@@ -78,11 +144,33 @@ LogLinInactivation <- R6::R6Class(
 # inact_model$simulate(1000) %>% hist()
 # inact_model$simulations
 
-
-
-
-
-
-
-
-
+## test weibull
+# 
+# WeibullInactivation$new("Weibull")
+# 
+# logN0 <- Normal$new("logN0")$
+#     map_input("mu", Constant$new("mu", 3))$
+#     map_input("sigma", Constant$new("sigma", 0.5))
+# 
+# log_delta <- Normal$new("log_delta")$
+#     map_input("mu", Constant$new("mu", 1))$
+#     map_input("sigma", Constant$new("sigma", 0.2))
+# 
+# t <- Constant$new("t", 7)
+# 
+# beta <- Constant$new("beta", 0.8)
+# 
+# 
+# weibull_model <- WeibullInactivation$new("Weibull")$
+#   map_input("beta", beta)$
+#   map_input("t", t)$
+#   map_input("logN0", logN0)$
+#   map_input("log_delta", log_delta)
+# 
+# 
+# 
+# weibull_model$depends_on
+# 
+# weibull_model$simulate(1000)%>%hist()
+# weibull_model$simulations
+# plot_model(weibull_model)
