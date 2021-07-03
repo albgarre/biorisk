db3d672326ab83004aa96c58f725bdd3ae4e0c99
albgarre <garre.alberto@gmail.com>
2021-07-03 13:50:37 +0200

    Implemented the secondary Peleg model

--- R/inactivation_secondary.R
+++ R/inactivation_secondary.R
@@ -59,6 +59,65 @@ Dz_model <- R6::R6Class(
 
 )
 
+#' R6 class describing the secondary model for the Peleg model
+#'
+#' @details
+#' A risk module describing the secondary model of the Peleg model. It has 3 inputs:
+#' k_b, temperature, temp_crit.
+#'
+#' @export
+#'
+Peleg_secondary <- R6::R6Class(
+  classname = "Peleg_secondary",
+  inherit = RiskModule,
+  public = list(
+
+    initialize = function(name,
+                          units = NA,
+                          output_unit = NA) {
+
+      super$initialize(name,
+                       input_names = c("temperature", "k_b", "temp_crit"),
+                       units = units,
+                       module_type = "secondary",
+                       output_var = "b",
+                       output_unit = output_unit)
+
+    },
+
+
+    #' @description
+    #' Simulates the module
+    #' @param niter Number of Monte Carlo simulations.
+    #' @return the output of the module
+    #'
+    simulate = function(niter) {
+
+      ## Do the simulations (recursively)
+
+      sims <- tibble::tibble(
+        temperature = self$depends_on$temperature$simulate(niter),
+        k_b = self$depends_on$k_b$simulate(niter),
+        temp_crit = self$depends_on$temp_crit$simulate(niter),
+      ) %>%
+        dplyr::mutate(
+          b = log(1 + exp(k_b * (temperature - temp_crit)))
+        )
+
+      ## Save the results of the simulations
+
+      self$simulations <- sims
+
+      ## Return
+
+      sims[[self$output]]
+
+    }
+
+  )
+
+)
+
 ## tests
 #
 ## tests
@@ -107,6 +166,57 @@ Dz_model <- R6::R6Class(
 # inact_model$simulate(1000) %>%
 #   hist()
 # inact_model$simulations
+
+
+## Test of the Peleg model
+
+# library(biorisk)
+# library(tidyverse)
+#
+# k_b <- LogNormal$new("k_b")$
+#   map_input("mu_log10", Constant$new("mu_log10", 0))$
+#   map_input("sigma_log10", Constant$new("simga_log10", .01))
+#
+# temperature <- Normal$new("temp")$
+#   map_input("mu", Constant$new("mu_temp", 60))$
+#   map_input("sigma", Constant$new("sigma_temp", 2))
+#
+# temp_crit <- Normal$new("temp_crit")$
+#   map_input("mu", Constant$new("mu_Tcr", 55))$
+#   map_input("sigma", Constant$new("sigma_Tcr", 1))
+#
+#
+# model_b <- Peleg_secondary$new("secondary_b")$
+#   map_input("k_b", k_b)$
+#   map_input("temperature", temperature)$
+#   map_input("temp_crit", temp_crit)
+#
+# model_b$simulate(1000)
+# plot_model(model_b)
+#
+#
+# time <- Constant$new("Time", 30)
+#
+# logN0 <- Normal$new("logN0")$
+#   map_input("mu", Constant$new("mu_logN0", 2))$
+#   map_input("sigma", Constant$new("sigma_logN0", 0.5))
+#
+# n = Normal$new("n")$
+#   map_input("mu", Constant$new("mu_n", 1))$
+#   map_input("sigma", Constant$new("sigma_n", .1))
+#
+#
+# inact_model <- PelegInactivation$new("Peleg")$
+#   map_input("n", n)$
+#   map_input("b", model_b)$
+#   map_input("t", t)$
+#   map_input("logN0", logN0)
+#
+#
+# inact_model$simulate(10000) %>%
+#   hist()
+# plot_model(inact_model)
+
 
 
 
