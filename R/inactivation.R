1c8c0b2e623e174a16c41fe1a0055ef2ad06d7ce
albgarre <garre.alberto@gmail.com>
2025-02-20 09:27:27 +0100

    Implemented the Geeraerd model without tail

--- R/inactivation.R
+++ R/inactivation.R
@@ -163,7 +163,7 @@ LogLinInactivation <- R6::R6Class(
 
 )
 
-#' R6 class describing the Weibull inactivation
+#' R6 class describing a Weibull primary inactivation
 #'
 #' @details
 #' A risk element describing Weibullian inactivation. It has 4 inputs: logN0, t
@@ -248,7 +248,7 @@ WeibullInactivation <- R6::R6Class(
 
 )
 
-#' R6 class describing the Weibull inactivation
+#' R6 class describing a Weibull inactivation model
 #'
 #' @details
 #' A risk element describing Weibullian inactivation using the parameterization by
@@ -430,10 +430,10 @@ TrilinearInactivation <- R6::R6Class(
 
 )
 
-#' R6 class describing Tri-linear inactivation
+#' R6 class describing Geeraerd inactivation model
 #'
 #' @details
-#' A risk element describing Tri-linear inactivation. It has 5 inputs: logN0, t, D, SL and Nres.
+#' A risk element describing the Geeraerd inactivation model. It has 5 inputs: logN0, t, D, SL and Nres.
 #'
 #' @export
 #'
@@ -519,10 +519,11 @@ GeeraerdInactivation <- R6::R6Class(
 
 )
 
-#' R6 class describing Tri-linear inactivation
+#' R6 class describing Geeraerd inactivation model without shoulder
 #'
 #' @details
-#' A risk element describing Tri-linear inactivation. It has 5 inputs: logN0, t, D, SL and Nres.
+#' A risk element describing Geeraerd inactivation model without shouler.
+#' It has 4 inputs: logN0, t, D and Nres.
 #'
 #' @export
 #'
@@ -607,202 +608,96 @@ GeeraerdInactivation_noSL <- R6::R6Class(
 
 )
 
+#' R6 class describing Geeraerd inactivation model without shoulder
+#'
+#' @details
+#' A risk element describing Geeraerd inactivation model without shouler.
+#' It has 4 inputs: logN0, t, D and Nres.
+#'
+#' @export
+#'
+GeeraerdInactivation_noTail <- R6::R6Class(
+  classname = "GeeraerdInactivation_noTail",
+  inherit = ContinuousElement,
+  public = list(
 
+    initialize = function(name,
+                          units = NA,
+                          output_unit = NA) {
 
-# ## tests
-#
-# library(biorisk)
-# library(tidyverse)
-#
-# time <- Constant$new("Time", 30)
-#
-# D <- LogNormal$new("D")$
-#   map_input("mu_log10", Constant$new("mu_logD", 1))$
-#   map_input("sigma_log10", Constant$new("sigma_logD", 0.2))
-#
-# logN0 <- Normal$new("logN0")$
-#   map_input("mu", Constant$new("mu_logN0", 2))$
-#   map_input("sigma", Constant$new("sigma_logN0", 0.5))
-#
-# inact_model <- LogLinInactivation$new("Inactivation")$
-#   map_input("t", time)$
-#   map_input("D", D)$
-#   map_input("logN0", logN0)
-#
-# inact_model$simulate(1000)
-# inact_model$histogram()
-#
-# ##
-#
-# # Dref <- LogNormal$new("Dref")$
-# #   map_input("mu_log10", Constant$new("mu_log10", .1))$
-# #   map_input("sigma_log10", Constant$new("simga_log10", .01))
-# #
-# # temperature <- Normal$new("temp")$
-# #   map_input("mu", Constant$new("mu_temp", 60))$
-# #   map_input("sigma", Constant$new("sigma_temp", 2))
-# #
-# # z <- Normal$new("z")$
-# #   map_input("mu", Constant$new("mu_z", 5))$
-# #   map_input("sigma", Constant$new("sigma_z", .2))
-# #
-# # Tref <- Constant$new("Tref", 60)
-# #
-# # model_D <- Dz_model$new("secondary_D")$
-# #   map_input("Dref", Dref)$
-# #   map_input("temperature", temperature)$
-# #   map_input("z", z)$
-# #   map_input("Tref", Tref)
-# #
-# # inact_model <- LogLinInactivation$new("Inactivation")$
-# #   map_input("t", time)$
-# #   map_input("D", model_D)$
-# #   map_input("logN0", logN0)
-# #
-# # plot_model(inact_model)
-# #
-# # inact_model$simulate(100) %>% hist()
-# #
-# # model_D$simulations$logD %>% hist()
-#
-# #
-# # plot_model(inact_model)
-#
-# ## test peleg
-#
-# #
-# logN0 <- Normal$new("logN0")$
-#   map_input("mu", Constant$new("mu_logN", 3))$
-#   map_input("sigma", Constant$new("sigma_logN", 0.5))
-#
-# b <- Normal$new("b")$
-#   map_input("mu", Constant$new("mu", .1))$
-#   map_input("sigma", Constant$new("sigma", 0.02))
-#
-# t <- Constant$new("t", 7)
-# n <- Normal$new("n")$
-#   map_input("mu", Constant$new("mu_n", 1))$
-#   map_input("sigma", Constant$new("sigma_n", .1))
-#
-#
-# aa <- PelegInactivation$new("Peleg")$
-#   map_input("n", n)$
-#   map_input("b", b)$
-#   map_input("t", t)$
-#   map_input("logN0", logN0)
-#
-# aa$simulate(10000)
-# aa$density_plot()
-#
-#
-#
-#
-# ## test weibull
-#
-# logN0 <- Normal$new("logN0")$
-#     map_input("mu", Constant$new("mu_logN", 3))$
-#     map_input("sigma", Constant$new("sigma_logN", 0.5))
-#
-# delta <- LogNormal$new("log_delta")$
-#     map_input("mu_log10", Constant$new("mu", 1))$
-#     map_input("sigma_log10", Constant$new("sigma", 0.2))
-#
-# t <- Constant$new("t", 7)
-#
-# beta <- Constant$new("beta", 0.8)
-#
-#
-# weibull_model <- WeibullInactivation$new("Weibull")$
-#   map_input("beta", beta)$
-#   map_input("t", t)$
-#   map_input("logN0", logN0)$
-#   map_input("delta", delta)
-#
-# weibull_model$simulate(1000)
-# weibull_model$boxplot()
-#
+      super$initialize(name,
+                       input_names = c("t", "logN0", "D", "SL"),
+                       input_types = list(t = "continuous",
+                                          D = "continuous",
+                                          SL = "continuous",
+                                          logN0 = "continuous"),
+                       units = units,
+                       element_type = "inactivation",
+                       output_var = "logN",
+                       output_unit = output_unit,
+                       level = 0)
 
-## test tri-linear
-#
-# library(biorisk)
-# library(tidyverse)
-#
-# time <- Normal$new("t")$
-#   map_input("mu", Constant$new("mu_t", 5))$
-#   map_input("sigma", Constant$new("sigma_logN0", 2))
-#
-# D <- LogNormal$new("D")$
-#   map_input("mu_log10", Constant$new("mu_logD", .1))$
-#   map_input("sigma_log10", Constant$new("sigma_logD", 0.02))
-#
-# logN0 <- Constant$new("logN0", 6)
-# logNres <- Constant$new("logNres", 3)
-# SL <- Constant$new("SL", 5)
-#
-# inact_model <- TrilinearInactivation$new("Inactivation")$
-#   map_input("t", time)$
-#   map_input("D", D)$
-#   map_input("logN0", logN0)$
-#   map_input("logNres", logNres)$
-#   map_input("SL", SL)
-#
-# plot_model(inact_model)
-# inact_model$simulate(1000)
-# inact_model$histogram()
-#
-#
-# plot_model(inact_model)
-#
-# time <- Normal$new("t")$
-#   map_input("mu", Constant$new("mu_t", 5))$
-#   map_input("sigma", Constant$new("sigma_logN0", 2))
-#
-# D <- LogNormal$new("D")$
-#   map_input("mu_log10", Constant$new("mu_logD", .1))$
-#   map_input("sigma_log10", Constant$new("sigma_logD", 0.02))
-#
-# logN0 <- Constant$new("logN0", 6)
-# logNres <- Constant$new("logNres", 3)
-# SL <- Constant$new("SL", 5)
-#
-# inact_model <- GeeraerdInactivation$new("Inactivation")$
-#   map_input("t", time)$
-#   map_input("D", D)$
-#   map_input("logN0", logN0)$
-#   map_input("logNres", logNres)$
-#   map_input("SL", SL)
-#
-# plot_model(inact_model)
-# inact_model$simulate(1000)
-# inact_model$histogram()
-#
-#
-# inact_model$point_estimate()
-#
-# time <- Normal$new("t")$
-#   map_input("mu", Constant$new("mu_t", 5))$
-#   map_input("sigma", Constant$new("sigma_logN0", 2))
-#
-# D <- LogNormal$new("D")$
-#   map_input("mu_log10", Constant$new("mu_logD", .1))$
-#   map_input("sigma_log10", Constant$new("sigma_logD", 0.02))
-#
-# logN0 <- Constant$new("logN0", 6)
-# logNres <- Constant$new("logNres", 3)
-# SL <- Constant$new("SL", 5)
-#
-# inact_model <- GeeraerdInactivation_noSL$new("Inactivation")$
-#   map_input("t", time)$
-#   map_input("D", D)$
-#   map_input("logN0", logN0)$
-#   map_input("logNres", logNres)
-#
-# plot_model(inact_model)
-# inact_model$simulate(1000)
-# inact_model$histogram()
-#
-#
-# inact_model$point_estimate()
+    },
+
+    #' @description
+    #' Returns the expected value
+    #'
+    point_estimate = function() {
+
+      t <- self$depends_on$t$point_estimate()
+      D <- self$depends_on$D$point_estimate()
+      SL <- self$depends_on$SL$point_estimate()
+      logN0 <- self$depends_on$logN0$point_estimate()
+
+      k <- log(10)/D
+
+      N <- 10^logN0 * exp(-k*t) * exp(k*SL) / ( 1 + ( exp(k*SL) - 1 )*exp(-k*t) )
+      log10(N)
+
+    }
+  ),
+
+  private = list(
+
+    update_output = function(niter) {
+
+      sims <- self$simulations %>%
+        dplyr::mutate(
+          k = log(10)/D,
+          N = 10^logN0 * exp(-k*t) * exp(k*SL) / ( 1 + ( exp(k*SL) - 1 )*exp(-k*t) ),
+          logN = log10(N)
+        )
+
+      self$simulations <- sims
+
+    },
+
+    update_output_level = function(niter0, iter1 = 1, level = 0) {
+
+      if (self$level > level) {
+        niter0 <- 1
+      }
+
+      sims <- self$simulations_multi[[iter1]] %>%
+        dplyr::mutate(
+          k = log(10)/D,
+          N = 10^logN0 * exp(-k*t) * exp(k*SL) / ( 1 + ( exp(k*SL) - 1 )*exp(-k*t) ),
+          logN = log10(N)
+        )
+
+      ## Save it
+
+      self$simulations_multi[[iter1]] <- sims
+
+      ## Return the output
+
+      invisible(sims[[self$output]])
+
+    }
+
+  )
+
+)
 
 
 
